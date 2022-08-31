71364c8aebdf4855960a1bd175304cd78f29caf4
Nandkumar Jawale <jawale.nandu@gmail.com>
2022-08-31 19:50:42 +0530

    added service annotation

--- products/src/main/java/com/github/jawale/products/service/ProductService.java
+++ products/src/main/java/com/github/jawale/products/service/ProductService.java
@@ -6,9 +6,11 @@ import com.github.jawale.products.model.Product;
 import com.github.jawale.products.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
